955f43b2dd0e9b205661af534aa81ad035b5dd4b
priyansh0401 <priyansh0401@icloud.com>
2025-01-21 22:11:32 +0530

    a

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const connectDB = require('./utils/conn')
+const connectDB = require('../utils/conn')
 const cors = require('cors')
 
 const app = express()
@@ -19,15 +19,15 @@ app.use(cors());
 
 app.use(express.json({ extended: false }));
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/student', require('./routes/studentRoutes'));
-app.use('/api/admin', require('./routes/adminRoutes'));
-app.use('/api/complaint', require('./routes/complaintRoutes'));
-app.use('/api/invoice', require('./routes/invoiceRoutes'));
-app.use('/api/messoff', require('./routes/messoffRoutes'));
-app.use('/api/request', require('./routes/requestRoutes'));
-app.use('/api/attendance', require('./routes/attendanceRoutes'));
-app.use('/api/suggestion', require('./routes/suggestionRoutes'));
+app.use('/api/auth', require('../routes/authRoutes'));
+app.use('/api/student', require('../routes/studentRoutes'));
+app.use('/api/admin', require('../routes/adminRoutes'));
+app.use('/api/complaint', require('../routes/complaintRoutes'));
+app.use('/api/invoice', require('../routes/invoiceRoutes'));
+app.use('/api/messoff', require('../routes/messoffRoutes'));
+app.use('/api/request', require('../routes/requestRoutes'));
+app.use('/api/attendance', require('../routes/attendanceRoutes'));
+app.use('/api/suggestion', require('../routes/suggestionRoutes'));
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`);
